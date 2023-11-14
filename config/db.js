8d766492061da7ba531ce858494870249466f022
AHanif09 <hanifahmad876@gmail.com>
2023-11-14 14:55:59 +0700

    merubah link database dari local ke cloud

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 
 const mongoose = require('mongoose');
 
-const DB_URL = process.env.DB_URL || "mongodb://127.0.0.1:27017/todo"
+const DB_URL = process.env.DB_URL || "mongodb+srv://hanifresearch01:hanif@cluster0.vhxgn1u.mongodb.net/todo"  //"mongodb://127.0.0.1:27017/todo"
 
 const db = mongoose.connect(DB_URL)
 
